6953ab664356a873d9be621a76f1c0d2e1598780
mchome <280604399@qq.com>
2018-02-22 15:05:32 +0800

    initial commit

---
+++ example/README.md
@@ -0,0 +1,8 @@
+# flutter_colorpicker_example
+
+Demonstrates how to use the flutter_colorpicker plugin.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](http://flutter.io/).
---
+++ lib/flutter_colorpicker.dart
@@ -0,0 +1,11 @@
+library flutter_colorpicker;
+
+import 'package:flutter/widgets.dart';
+
+class ColorPicker extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container();
+  }
+
+}
