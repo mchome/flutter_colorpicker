190f8d4c0f325336065e81dfb6e6e77920723dd9
mchome <280604399@qq.com>
2020-02-16 16:40:59 +0800

    init

---
+++ flutter_service_worker.js
@@ -0,0 +1,38 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"/assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/LICENSE": "dc3fad2e02aa48452780b5fb994e16c5",
+"/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/index.html": "b6a04d24d135920059681f55c5b495b6",
+"/main.dart.js": "73b759b14a3231115c8a8186369c7ead",
+"/manifest.json": "00e0b69b49487ce4f9ff0c5fac8fda49"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
