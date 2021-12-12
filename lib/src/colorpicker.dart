7c88644f94ae8bd82ce916cb1079a82a8a4812fd
mchome <fnatic.roach@gmail.com>
2021-12-12 10:54:02 +0800

    code clean up

--- lib/src/colorpicker.dart
+++ lib/src/colorpicker.dart
@@ -4,7 +4,6 @@
 
 library hsv_picker;
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/src/palette.dart';
 import 'package:flutter_colorpicker/src/utils.dart';
